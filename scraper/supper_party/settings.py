8c2ff7f13b3ec313ceee9eeb1251f1977cb83c2c
Colin Burr <colin.a.burr@gmail.com>
2020-10-11 16:16:20 -0400

    Use docker-compose

--- scraper/supper_party/settings.py
+++ scraper/supper_party/settings.py
@@ -63,7 +63,7 @@ ROBOTSTXT_OBEY = False
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-   'supper_party.pipelines.SupperPartyPipeline': 300,
+    "supper_party.pipelines.SupperPartyPipeline": 300,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
