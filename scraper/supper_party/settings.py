38497652cdee3ffe5a849f6ded6117bfddedf518
Colin Burr <colin.a.burr@gmail.com>
2020-10-11 16:36:23 -0400

    Add pipeline to Mongo

--- scraper/supper_party/settings.py
+++ scraper/supper_party/settings.py
@@ -86,3 +86,6 @@ ITEM_PIPELINES = {
 # HTTPCACHE_DIR = 'httpcache'
 # HTTPCACHE_IGNORE_HTTP_CODES = []
 # HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+MONGO_URI = "mongo:27017"
+MONGO_DATABASE = "supper_party"
