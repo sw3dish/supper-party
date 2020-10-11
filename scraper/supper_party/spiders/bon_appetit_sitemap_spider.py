e75c6aad66a66bd6b764fadacc48012f289dcbf9
Colin Burr <colin.a.burr@gmail.com>
2020-10-11 17:12:40 -0400

    Crawl only recipes, not slideshows

--- scraper/supper_party/spiders/bon_appetit_sitemap_spider.py
+++ scraper/supper_party/spiders/bon_appetit_sitemap_spider.py
@@ -1,6 +1,5 @@
 from supper_party.items import RecipeItem
 
-import json
 import scrapy
 
 
@@ -8,19 +7,11 @@ class BonAppetitSitemapSpider(scrapy.spiders.SitemapSpider):
     name = "bon-appetit-sitemap-spider"
     allowed_domains = ["bonappetit.com"]
     sitemap_urls = ["https://bonappetit.com/sitemap.xml"]
-
-    sitemap_follow = [
-        "https://www.bonappetit.com/sitemap.xml\?year=2020&month=10&week=2"
-    ]
-
-    sitemap_rules = [("/recipe", "parse_recipe")]
+    sitemap_rules = [("/recipe/", "parse_recipe")]
 
     def parse_recipe(self, response):
         schema_json = response.xpath(
             "//script[@type='application/ld+json']/text()"
         ).get()
-        # recipe_schema = json.loads(
-        #     schema_json.replace("\r", "\\r").replace("\t", "\\t")
-        # )
         item = RecipeItem(url=response.url, json=schema_json)
         yield item
